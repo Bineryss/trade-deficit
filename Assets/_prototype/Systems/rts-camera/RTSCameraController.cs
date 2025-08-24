6cf8e5047dd0cd9fc7d1b411d1ed3ff29b3f7daa
Sebastian Otto <44194014+Bineryss@users.noreply.github.com>
2025-08-25 01:59:00 +0200

    refactor

--- Assets/_prototype/Systems/rts-camera/RTSCameraController.cs
+++ Assets/_prototype/Systems/rts-camera/RTSCameraController.cs
@@ -1,202 +1,123 @@
-using System;
-using RTSCamera;
 using Unity.Cinemachine;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-public class RTSCameraController : MonoBehaviour
+namespace RTSCamera
 {
-    [Header("Components")]
-    [SerializeField] private Transform trackingPoint;
-    [SerializeField] private PlayerInput playerInput;
-    [SerializeField] private CinemachineOrbitalFollow orbitalFollow;
-    [SerializeField] private Camera playerCamera;
-
-    [Header("Movement")]
-    [SerializeField] private float moveSpeed = 20f;
-    [SerializeField] private float acceleration = 10f;
-    [SerializeField] private float deceleration = 20f;
-    private Vector3 velocity = Vector3.zero;
-    [Header("Zoom")]
-    [SerializeField] private float zoomSpeed = 20f;
-    [SerializeField] private float zoomSmoothing = 5f;
-    [SerializeField] private AnimationCurve zoomSpeedCurve;
-    [SerializeField] private float mouseWheelZoomSpeedMultiplier = 5f;
-    private float currentZoomSpeed;
-    [Header("Orbit")]
-    [SerializeField] private float orbitSensitivity = 0.5f;
-    [SerializeField] private float orbitSmoothing = 5f;
-    private Vector2 orbitVelocity = Vector2.zero;
-
-    private RTSCameraInputs inputActions;
-    private Vector2 moveInput;
-    private Vector2 rotateInput;
-    private bool middleClickInput;
-    [SerializeField] private float zoomInput;
-    private bool isGamePad;
-
-    void OnEnable()
+    [RequireComponent(typeof(RTSCameraInputProvider))]
+    public class RTSCameraController : MonoBehaviour
     {
-        inputActions.camera.move.performed += OnMove;
-        inputActions.camera.move.canceled += OnMove;
+        [Header("Components")]
+        [SerializeField] private Transform trackingPoint;
+        [SerializeField] private CinemachineOrbitalFollow orbitalFollow;
+        [SerializeField] private Camera playerCamera;
 
-        inputActions.camera.look.performed += OnRotate;
-        inputActions.camera.look.canceled += OnRotate;
+        [Header("Movement")]
+        [SerializeField] private float moveSpeed = 20f;
+        [SerializeField] private float acceleration = 10f;
+        [SerializeField] private float deceleration = 20f;
+        private Vector3 velocity = Vector3.zero;
+        [Header("Zoom")]
+        [SerializeField] private float zoomSpeed = 20f;
+        [SerializeField] private float zoomSmoothing = 5f;
+        [SerializeField] private AnimationCurve zoomSpeedCurve;
+        private float currentZoomSpeed;
+        [Header("Orbit")]
+        [SerializeField] private float orbitSensitivity = 0.5f;
+        [SerializeField] private float orbitSmoothing = 5f;
+        private Vector2 orbitVelocity = Vector2.zero;
+        [Header("Input")]
+        [SerializeField] private RTSCameraInputProvider inputReader;
 
-        inputActions.camera.togglerotate.started += OnToggleRotate;
-        inputActions.camera.togglerotate.canceled += OnToggleRotate;
-
-        inputActions.camera.zoom.performed += OnZoom;
-        inputActions.camera.zoom.canceled += OnZoom;
-
-        playerInput.onControlsChanged += HandleControlsChanged;
-    }
-
-    void OnDisable()
-    {
-        inputActions.camera.move.performed -= OnMove;
-        inputActions.camera.move.canceled -= OnMove;
-
-        inputActions.camera.look.performed -= OnRotate;
-        inputActions.camera.look.canceled -= OnRotate;
-
-        inputActions.camera.togglerotate.started -= OnToggleRotate;
-        inputActions.camera.togglerotate.canceled -= OnToggleRotate;
-
-        inputActions.camera.zoom.performed -= OnZoom;
-        inputActions.camera.zoom.canceled -= OnZoom;
-
-        playerInput.onControlsChanged -= HandleControlsChanged;
-    }
-
-    void Awake()
-    {
-        inputActions = new RTSCameraInputs();
-        inputActions.Enable();
-        if (playerInput == null)
+        void OnEnable()
         {
-            playerInput = GetComponent<PlayerInput>();
+            if (playerCamera == null)
+            {
+                playerCamera = Camera.main;
+            }
         }
 
-        if (playerCamera == null)
+        void Update()
         {
-            playerCamera = Camera.main;
-        }
-    }
-
-    void Update()
-    {
-        float deltaTime = Time.unscaledDeltaTime;
-        UpdateMovement(deltaTime);
-        UpdateOrbit(deltaTime);
-        UpdateZoom(deltaTime);
-    }
-
-    private void HandleControlsChanged(PlayerInput input)
-    {
-        isGamePad = inputActions.GamepadScheme.name.Equals(input.currentControlScheme);
-    }
-    private void UpdateOrbit(float deltaTime)
-    {
-        orbitVelocity = Vector2.MoveTowards(orbitVelocity, orbitSensitivity * rotateInput, deltaTime * orbitSmoothing);
-
-        InputAxis horizontalAxis = orbitalFollow.HorizontalAxis;
-        InputAxis verticalAxis = orbitalFollow.VerticalAxis;
-
-        horizontalAxis.Value += orbitVelocity.x;
-        verticalAxis.Value -= orbitVelocity.y;
-
-        horizontalAxis.Value = ShiftValueInBounds(horizontalAxis);
-        verticalAxis.Value = ShiftValueInBounds(verticalAxis);
-
-        orbitalFollow.HorizontalAxis = horizontalAxis;
-        orbitalFollow.VerticalAxis = verticalAxis;
-    }
-
-    private float ShiftValueInBounds(InputAxis axis)
-    {
-        if (axis.Wrap)
-        {
-            float normalized = axis.Value + Mathf.Abs(axis.Range.x);
-            float wrapped = Mathf.Repeat(normalized, Mathf.Abs(axis.Range.x - axis.Range.y));
-            return wrapped - Mathf.Abs(axis.Range.x);
+            float deltaTime = Time.unscaledDeltaTime;
+            UpdateMovement(deltaTime, inputReader.MoveInput);
+            UpdateOrbit(deltaTime, inputReader.RotateInput);
+            UpdateZoom(deltaTime, inputReader.ZoomInput);
         }
 
-
-        return Mathf.Clamp(axis.Value, axis.Range.x, axis.Range.y);
-    }
-
-    private void UpdateMovement(float deltaTime)
-    {
-        Vector3 forward = playerCamera.transform.forward;
-        forward.y = 0;
-        forward.Normalize();
-
-        Vector3 right = playerCamera.transform.right;
-        right.y = 0;
-        right.Normalize();
-
-
-        float zoomFactorNormalized = Mathf.InverseLerp(orbitalFollow.RadialAxis.Range.x, orbitalFollow.RadialAxis.Range.y, orbitalFollow.RadialAxis.Value);
-        float zoomSpeedMultiplier = Mathf.Lerp(0.1f, 1f, zoomFactorNormalized);
-        Vector3 targetVelocity = moveSpeed * zoomSpeedMultiplier * new Vector3(moveInput.x, 0, moveInput.y);
-
-        if (moveInput.sqrMagnitude > 0.1f)
+        private void UpdateOrbit(float deltaTime, Vector2 rotateInput)
         {
-            velocity = Vector3.Lerp(velocity, targetVelocity, acceleration * deltaTime);
-        }
-        else
-        {
-            velocity = Vector3.Lerp(velocity, Vector3.zero, deceleration * deltaTime);
+            orbitVelocity = Vector2.MoveTowards(orbitVelocity, orbitSensitivity * rotateInput, deltaTime * orbitSmoothing);
+
+            InputAxis horizontalAxis = orbitalFollow.HorizontalAxis;
+            InputAxis verticalAxis = orbitalFollow.VerticalAxis;
+
+            horizontalAxis.Value += orbitVelocity.x;
+            verticalAxis.Value -= orbitVelocity.y;
+
+            horizontalAxis.Value = ShiftValueInBounds(horizontalAxis);
+            verticalAxis.Value = ShiftValueInBounds(verticalAxis);
+
+            orbitalFollow.HorizontalAxis = horizontalAxis;
+            orbitalFollow.VerticalAxis = verticalAxis;
         }
 
-        Vector3 motion = velocity * deltaTime;
-        trackingPoint.position += forward * motion.z + right * motion.x;
-    }
-    private void UpdateZoom(float deltaTime)
-    {
-        InputAxis axis = orbitalFollow.RadialAxis;
-
-        float targetZoomSpeed = 0;
-        float zoomMultiplier = zoomSpeedCurve.Evaluate(Mathf.InverseLerp(axis.Range.x, axis.Range.y, axis.Value)) * zoomSpeed;
-
-        if (Mathf.Abs(zoomInput) >= 0.01f)
+        private float ShiftValueInBounds(InputAxis axis)
         {
-            targetZoomSpeed = zoomInput * zoomMultiplier;
-        }
-        currentZoomSpeed = Mathf.Lerp(currentZoomSpeed, targetZoomSpeed, zoomSmoothing * deltaTime);
+            if (axis.Wrap)
+            {
+                float normalized = axis.Value + Mathf.Abs(axis.Range.x);
+                float wrapped = Mathf.Repeat(normalized, Mathf.Abs(axis.Range.x - axis.Range.y));
+                return wrapped - Mathf.Abs(axis.Range.x);
+            }
 
-        axis.Value -= currentZoomSpeed * deltaTime;
-        axis.Value = Mathf.Clamp(axis.Value, axis.Range.x, axis.Range.y);
 
-        orbitalFollow.RadialAxis = axis;
-    }
-    private void OnMove(InputAction.CallbackContext context)
-    {
-        moveInput = context.ReadValue<Vector2>();
-    }
-    private void OnRotate(InputAction.CallbackContext context)
-    {
-        if (!isGamePad && !middleClickInput)
-        {
-            rotateInput = Vector2.zero;
-            return;
+            return Mathf.Clamp(axis.Value, axis.Range.x, axis.Range.y);
         }
-        rotateInput = context.ReadValue<Vector2>();
-    }
-    private void OnZoom(InputAction.CallbackContext context)
-    {
-        if (isGamePad)
+
+        private void UpdateMovement(float deltaTime, Vector2 moveInput)
         {
-            zoomInput = Mathf.Clamp(context.ReadValue<float>(), -1f, 1f);
+            Vector3 forward = playerCamera.transform.forward;
+            forward.y = 0;
+            forward.Normalize();
+
+            Vector3 right = playerCamera.transform.right;
+            right.y = 0;
+            right.Normalize();
+
+
+            float zoomFactorNormalized = Mathf.InverseLerp(orbitalFollow.RadialAxis.Range.x, orbitalFollow.RadialAxis.Range.y, orbitalFollow.RadialAxis.Value);
+            float zoomSpeedMultiplier = Mathf.Lerp(0.1f, 1f, zoomFactorNormalized);
+            Vector3 targetVelocity = moveSpeed * zoomSpeedMultiplier * new Vector3(moveInput.x, 0, moveInput.y);
+
+            if (moveInput.sqrMagnitude > 0.1f)
+            {
+                velocity = Vector3.Lerp(velocity, targetVelocity, acceleration * deltaTime);
+            }
+            else
+            {
+                velocity = Vector3.Lerp(velocity, Vector3.zero, deceleration * deltaTime);
+            }
+
+            Vector3 motion = velocity * deltaTime;
+            trackingPoint.position += forward * motion.z + right * motion.x;
         }
-        else
+        private void UpdateZoom(float deltaTime, float zoomInput)
         {
-            zoomInput = Mathf.Clamp(context.ReadValue<float>(), -1f, 1f) * mouseWheelZoomSpeedMultiplier;
+            InputAxis axis = orbitalFollow.RadialAxis;
+
+            float targetZoomSpeed = 0;
+            float zoomMultiplier = zoomSpeedCurve.Evaluate(Mathf.InverseLerp(axis.Range.x, axis.Range.y, axis.Value)) * zoomSpeed;
+
+            if (Mathf.Abs(zoomInput) >= 0.01f)
+            {
+                targetZoomSpeed = zoomInput * zoomMultiplier;
+            }
+            currentZoomSpeed = Mathf.Lerp(currentZoomSpeed, targetZoomSpeed, zoomSmoothing * deltaTime);
+
+            axis.Value -= currentZoomSpeed * deltaTime;
+            axis.Value = Mathf.Clamp(axis.Value, axis.Range.x, axis.Range.y);
+
+            orbitalFollow.RadialAxis = axis;
         }
-    }
-    private void OnToggleRotate(InputAction.CallbackContext context)
-    {
-        middleClickInput = context.ReadValueAsButton();
     }
 }
---
+++ Assets/_prototype/Systems/rts-camera/RTSCameraInputReader.cs
@@ -0,0 +1,96 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace RTSCamera
+{
+    [RequireComponent(typeof(PlayerInput))]
+    public class RTSCameraInputProvider : MonoBehaviour
+    {
+        [SerializeField] private PlayerInput playerInput;
+        [SerializeField] private float mouseWheelZoomSpeedMultiplier = 5f;
+
+        public Vector2 MoveInput { get; private set; }
+        public Vector2 RotateInput { get; private set; }
+        public float ZoomInput { get; private set; }
+
+        private bool isGamePad;
+        private RTSCameraInputs inputActions;
+        private bool middleClickInput;
+        void OnEnable()
+        {
+            if (playerInput == null)
+            {
+                playerInput = GetComponent<PlayerInput>();
+            }
+
+            inputActions ??= new RTSCameraInputs();
+            inputActions.Enable();
+
+            inputActions.camera.move.performed += OnMove;
+            inputActions.camera.move.canceled += OnMove;
+
+            inputActions.camera.look.performed += OnRotate;
+            inputActions.camera.look.canceled += OnRotate;
+
+            inputActions.camera.togglerotate.started += OnToggleRotate;
+            inputActions.camera.togglerotate.canceled += OnToggleRotate;
+
+            inputActions.camera.zoom.performed += OnZoom;
+            inputActions.camera.zoom.canceled += OnZoom;
+
+            playerInput.onControlsChanged += HandleControlsChanged;
+        }
+
+        void OnDisable()
+        {
+            inputActions.camera.move.performed -= OnMove;
+            inputActions.camera.move.canceled -= OnMove;
+
+            inputActions.camera.look.performed -= OnRotate;
+            inputActions.camera.look.canceled -= OnRotate;
+
+            inputActions.camera.togglerotate.started -= OnToggleRotate;
+            inputActions.camera.togglerotate.canceled -= OnToggleRotate;
+
+            inputActions.camera.zoom.performed -= OnZoom;
+            inputActions.camera.zoom.canceled -= OnZoom;
+
+            playerInput.onControlsChanged -= HandleControlsChanged;
+        }
+
+        private void HandleControlsChanged(PlayerInput input)
+        {
+            isGamePad = inputActions.GamepadScheme.name.Equals(input.currentControlScheme);
+        }
+
+        private void OnMove(InputAction.CallbackContext context)
+        {
+            MoveInput = context.ReadValue<Vector2>();
+        }
+        private void OnRotate(InputAction.CallbackContext context)
+        {
+            if (!isGamePad && !middleClickInput)
+            {
+                RotateInput = Vector2.zero;
+                return;
+            }
+            RotateInput = context.ReadValue<Vector2>();
+        }
+        private void OnZoom(InputAction.CallbackContext context)
+        {
+            if (isGamePad)
+            {
+                ZoomInput = Mathf.Clamp(context.ReadValue<float>(), -1f, 1f);
+            }
+            else
+            {
+                ZoomInput = Mathf.Clamp(context.ReadValue<float>(), -1f, 1f) * mouseWheelZoomSpeedMultiplier;
+            }
+        }
+        private void OnToggleRotate(InputAction.CallbackContext context)
+        {
+            middleClickInput = context.ReadValueAsButton();
+        }
+
+    }
+}
